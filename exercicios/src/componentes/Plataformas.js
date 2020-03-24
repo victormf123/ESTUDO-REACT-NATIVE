dc4f4bc7ee4fe095ce366adf5e323fc670926da7
Matheus Freitas <victormf.df@hotmail.com>
2020-03-24 08:29:10 -0300

    Validar Props, Eventos  e estados aplicação

---
+++ exercicios/src/componentes/Plataformas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Alert, ToastAndroid, Platform} from 'react-native';
+
+export default props => {
+  const notificar = msg => {
+    if (Platform.OS === 'android') {
+      ToastAndroid.show(msg, ToastAndroid.LONG);
+    } else {
+      Alert.alert('Informação', msg);
+    }
+  };
+  return <Button title="Plataform?" onPress={() => notificar('Parabéns')} />;
+};
